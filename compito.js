924433b26159acd283957e2e790def5d05bd42e9
Elisa Superchi <145764247+elisa-S-02@users.noreply.github.com>
2023-10-11 12:48:23 +0200

    Initial commit

---
+++ compito.js
@@ -0,0 +1,6 @@
+// 1)Un algoritmo è un insieme di step per risolvere un dato problema,
+//  se poi questi step siano semplici o estemamente complessi non inficia sulla definizione di algoritmo.
+
+//2)una variabile è una scatoletta che continene le informazioni per compiere un'istruzione.
+
+//3) undefined e null NON sono la stessa cosa.
